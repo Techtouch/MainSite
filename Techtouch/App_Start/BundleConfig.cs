39b1343c1adea225e5da41b3305e139052497f3a
Khristopher Dickson <xkhrisx@gmail.com>
2015-07-06 09:19:17 -0400

    edited social buttons, need to add api keys

--- Techtouch/App_Start/BundleConfig.cs
+++ Techtouch/App_Start/BundleConfig.cs
@@ -26,6 +26,12 @@ namespace Techtouch
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+            "~/Content/bootstrap.css",
+            "~/Content/site.css",
+            "~/Content/zocial.css"
+            ));
         }
     }
 }
