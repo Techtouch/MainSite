e6421fc6265facf1498df64e51422d0ff8713f4a
timo0060 <timo0060@gmail.com>
2015-07-09 09:46:35 -0400

    Added content to Home, About, and Contact

--- Techtouch/Controllers/HomeController.cs
+++ Techtouch/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Techtouch.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "A little Something About Us";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Have any Questions?";
 
             return View();
         }
