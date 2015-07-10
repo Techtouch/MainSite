b1857a10db1065e140d90ee9887c13b32342c45f
aalmacin <aldrinjerome19@gmail.com>
2015-07-10 05:01:58 +0000

    Added logo and updated homepage

--- Techtouch/Controllers/HomeController.cs
+++ Techtouch/Controllers/HomeController.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Techtouch.Models;
 
 namespace Techtouch.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
+        private TechtouchConnection db = new TechtouchConnection();
         public ActionResult Index()
         {
-            return View();
+            ViewBag.HeaderImage = "~/Content/Images/header.jpg";
+
+            var products = db.Products.ToList().OrderByDescending(m => m.product_price).Take(5);
+            return View(products);
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "A little Something About Us";
+            ViewBag.PanelHeading1 = "A little Something About Us";
+            ViewBag.PanelHeading2 = "A letter to you";
 
             return View();
         }
