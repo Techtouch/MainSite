4fe367939587371b40139a201803d1910009320b
aalmacin <aldrinjerome19@gmail.com>
2015-08-08 22:36:38 -0400

    Added angular to home page

---
+++ Techtouch/Content/js/home.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var myApp = angular.module("myApp", []);
+    var HomeCtrl = function ($scope, $http) {
+
+        var onError = function () {
+            $scope.error = "Unable to retrieve data.";
+        }
+
+        var bestProducts = function (response) {
+            $scope.bestProducts = response.data;
+        }
+
+        var cheapestProducts = function (response) {
+            $scope.cheapestProducts = response.data;
+        }
+
+        $http.get("/api/ProductsApi/GetBestProducts/6").then(bestProducts, onError);
+        $http.get("/api/ProductsApi/GetCheapestProducts/6").then(cheapestProducts, onError);
+    };
+
+    myApp.controller("HomeCtrl", ["$scope", "$http", HomeCtrl]);
+}());
